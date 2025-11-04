ed79dadb448faaeae43fed55914fca42bf374532
Kuldik <112172212+Kuldik@users.noreply.github.com>
2025-11-04 11:02:47 +0100

    fix: build #7

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const repo = 'Wargaming-home-task'
-
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: "/",
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    globals: true,
-    restoreMocks: true,
-    clearMocks: true,
-    fakeTimers: {
-      toFake: ['setTimeout', 'clearTimeout', 'Date'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') 
+  return {
+    plugins: [react()],
+    base: env.VITE_PUBLIC_BASE || '/',   
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./vitest.setup.ts'],
+      globals: true,
+      restoreMocks: true,
+      clearMocks: true,
+      fakeTimers: { toFake: ['setTimeout', 'clearTimeout', 'Date'] },
     },
-  },
-}));
+  }
+})
